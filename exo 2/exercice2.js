1ed96696b23f7e8e1a901f74d4279c28d075051d
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-01 10:11:17 +0100

    Modif

--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,9 +1,8 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-
-for( var i =56 ;  < ; )
-{
-    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
-}
+let i;
+for (i = 0; i < 56; i+=28) {
+    document.getElementById('monDiv').innerHTML += "Ma variable i vaux"+i+"<br><br>";
+        }
 
 
